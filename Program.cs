c4f8ef800baadc22e1dcbdb339d7700f2f2d6c6b
Wanderson Alves <wandersonalves1234@gmail.com>
2024-11-21 23:12:59 -0300

    "adicionadas as validações restantes"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "O do bom", imei: "7777777", memoria: 240);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone(numero: "54321", modelo: "O do melhor", imei: "6666666", memoria: 500);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo();
